71dac57fa229346b20c79e35a93c41fc1a3aa655
Allison Homem <mrs.allisonhomem@outlook.com>
2021-08-13 11:10:38 -0500

    fixed bugs

--- src/components/CharacterInfo.js
+++ src/components/CharacterInfo.js
@@ -34,25 +34,27 @@ const CharacterInfo = (props) => {
         },[]);
 
     //function to get starships info using axios and useEffect to prevent multiple data grabs
-    const theShips = (character.starships.length!==0) && character.starships.map(ship => {
-        useEffect(() => {
-            axios.get(ship)
-                 .then(res => {
-                    console.log(res);
-                 })
-                 .catch(err => {
-                    console.error('uh-oh... failed to retrieve data');
-                 })
-        }, []);
-    })
+    const theShips = [];
+    useEffect(() => {
+    (character.starships.length!==0) && character.starships.forEach(ship => {
+        axios.get(ship)
+             .then(res => {
+                theShips.push(res.data.model);
+             })
+             .catch(err => {
+                console.error('uh-oh... failed to retrieve data');
+             })
+        })
+    setStarships(theShips);
+    }, []);
 
     return(
         <div>
             <p>Born: {character.birth_year}</p>
             <p>Species: {species}</p>
             <p>Home Planet: {planet}</p>
-            <p>Stars in: {character.films.map((film) => {return `"${film}", `})}</p>
-            <p>Seen flying: {(character.starships.length !== 0) ? theShips.map(ship => {return `${ship}, `}):`never`}</p>
+            <p>Stars in: {character.films.map((film) => {return `"${film}" + `})}</p>
+            <p>Seen flying: {(character.starships.length !== 0) ? starships.map(ship => {return `${ship} + `}):`never`}</p>
         </div>
     );
 }
